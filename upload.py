941eac348db4cb0d1576ea65df99ece91c0fce5d
Kotak Biasa <muhnurfauzan73@gmail.com>
2024-12-31 10:02:00 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -7,24 +7,20 @@ def upload_files_in_directory(directory, api_id, api_hash, bot_token):
     with Client("deltarvx", api_id=api_id, api_hash=api_hash, bot_token=bot_token) as app:
         chat_id = 'KotakReVanced'  # Ganti dengan ID chat atau channel tujuan Anda
         base_url = f"https://github.com/kotakbiasa/TelegramZipUploader/blob/main/{directory}/"
-        sticker_file_id = 'CAACAgUAAxkBAAL5bmdzQCVh4hHgW2-fkVtnHJ4Ht8ETAAJRAgACPFM4VgSSYZ1RQ-Q_NgQ'  # ID or file path of the sticker
-
+        
         for root, _, files in os.walk(directory):
             for filename in files:
                 file_path = os.path.join(root, filename)
                 
                 # Menggunakan nama file dan URL sebagai caption saat mengunggahnya dengan markdown
                 file_url = os.path.join(base_url, filename)
-                caption = f"{filename}"
+                caption = f"`{filename}`"
                 
                 # Membuat tombol download dengan InlineKeyboardButton
                 button = InlineKeyboardButton(text="📦 Download", url=file_url)
                 reply_markup = InlineKeyboardMarkup([[button]])
                 
                 app.send_document(chat_id=chat_id, document=file_path, caption=caption, reply_markup=reply_markup)
-                
-                # Mengirim sticker setelah mengunggah file
-                app.send_sticker(chat_id=chat_id, sticker=sticker_file_id)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Upload files in a directory to Telegram.')
